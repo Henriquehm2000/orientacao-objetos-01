a20a1f4b6eb341a708e9ba34addd8a490b5babba
Henrique Melchior <hmghmghmghmg2000@gmail.com>
2020-02-19 22:02:29 -0300

    create test for rectangle class

--- src/main/java/exercicios/retangulo.java
+++ src/main/java/exercicios/retangulo.java
@@ -3,10 +3,11 @@ package exercicios;
 public class retangulo {
     public double base;
     public double altura;
-}
-public double calculararea(){
-    return base * altura;
-}
-public double calcularperimetro(){
-    return 2 * ( base + altura );
+
+    public double calculararea(){
+        return base * altura;
+    }
+    public double calcularperimetro(){
+        return 2 * ( base + altura );
+    }
 }
---
+++ src/test/java/exercicios/RetanguloTeste.java
@@ -0,0 +1,40 @@
+package exercicios;
+
+import exemplos.Quadrado;
+import org.junit.jupiter.api.Test;
+
+import static jdk.vm.ci.aarch64.AArch64.r1;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class RetanguloTeste {
+    @Test
+    public void criarObjetoRetangulo() {
+        retangulo r1 = new retangulo();
+        assertNotNull(r1);
+    }
+
+    @Test
+    public void presencaAtributoBase() {
+        retangulo r1 = new retangulo();
+        r1.base = 10.0;
+        assertNotNull(r1.base);
+    }
+
+    @Test
+    public void presencaAtributoAltura() {
+        retangulo r1 = new retangulo();
+        r1.altura = 15.0;
+        assertNotNull(r1.altura);
+    }
+
+    @Test
+    public void metodoCalcularPerimetro() {
+        retangulo r1 = new retangulo();
+        r1.base = 10.0;
+        r1.altura = 15.0;
+        Double valorEsperado = 50.0;
+        Double valorObtido = r1.calcularperimetro();
+        assertEquals(valorEsperado, valorObtido);
+    }
+}
